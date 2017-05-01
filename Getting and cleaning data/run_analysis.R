4e15b416ee0db0c4acb82da8ca2ffe8cb1891197
theoser2 <theo.sermet@gmail.com>
2017-05-01 15:12:21 +0200

    first version for coursera

---
+++ Getting and cleaning data/run_analysis.R
@@ -0,0 +1,15 @@
+
+# Downloading the data
+if(file.exist("data_coursera.zip")) file.remove("data_coursera.zip")
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile="data_coursera.zip")
+unzip("data_coursera.zip")
+setwd("UCI HAR Dataset")
+
+train_set <- read.csv("")
+test_set <- read.csv("")
+
+  
+
+# Removing files from your computer
+file.remove("data_coursera.zip")
+unlink("UCI HAR Dataset", recursive=T)
